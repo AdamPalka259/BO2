504eabdab37861fb61b2f6acb000dbf40d014542
Adam Palka <adam.palka259@gmail.com>
2024-11-25 00:56:28 +0100

    Zmiany w rand.py

--- rand.py
+++ rand.py
@@ -8,33 +8,45 @@ def generate_data(N: int, num_dormitories: int, num_departments: int) -> Tuple[
     # Students data
     students_years = [random.randint(1, 5) for _ in range(N)]  # Losowy rok studiów od 1 do 5
     students_disability = random.choices([0, 1, 2, 3], weights=[70, 15, 10, 5], k=N) # Stopień niepełnosprawności
-    students_priority_lists = [random.sample(range(1, num_dormitories + 1), num_dormitories) for _ in range(N)]  # Priorytety akademików
-    students_departments = [random.randint(1, num_departments) for _ in range(N)]  # Losowy wydział dla każdego studenta
+    students_priority_lists = [random.sample(range(num_dormitories), num_dormitories) for _ in range(N)]  # Priorytety akademików
+    students_departments = [random.randint(0, num_departments - 1) for _ in range(N)]  # Losowy wydział dla każdego studenta
 
     # Dormitories data
     dormitory_position = []
     dormitorys_capacity = []
-    for y in range(1, num_dormitories + 1):
+    for y in range(num_dormitories):
         dormitory_position.append((round(random.uniform(1, 50), 2), round(random.uniform(1, 50), 2)))  # Losowa pozycja akademika (x, y)
-        dormitorys_capacity.append(random.randint(50, 100))  # Losowa pojemność akademika
         
-    print('dormitorys_capacities')
-    print(dormitorys_capacity)
-    print('dormitorys_positions')
-    print(dormitory_position)
+    total_capacity = 0
+    while True:
+        for y in range(num_dormitories):
+            dormitorys_capacity.append(random.randint(50, 200))  # Losowa pojemność akademika
+            total_capacity += dormitorys_capacity[y]
+        
+        if total_capacity < N:
+            total_capacity = 0
+            dormitorys_capacity = []
+
+        else:
+            break
+        
+    # print('dormitorys_capacities')
+    # print(dormitorys_capacity)
+    # print('dormitorys_positions')
+    # print(dormitory_position)
 
     # Departments data
     departments_position = []
-    for y in range(1, num_departments + 1):
+    for y in range(num_departments):
         departments_position.append((round(random.uniform(1, 50), 2), round(random.uniform(1, 50), 2)))
-    print('departments_positions')
-    print(departments_position)
+    # print('departments_positions')
+    # print(departments_position)
 
     # Student_data
-    print("stud_data id rok niepel priorytet_akademikow   wydzial kordy_wydzialu")
-    for i in range(N):
-        x = students_departments[i]
-        print("student_id",i,"->",students_years[i],students_disability[i],students_priority_lists[i],students_departments[i],departments_position[x-1])
+    # print("stud_data id rok niepel priorytet_akademikow   wydzial kordy_wydzialu")
+    # for i in range(N):
+    #     x = students_departments[i]
+    #     print("student_id",i,"->",students_years[i],students_disability[i],students_priority_lists[i],students_departments[i],departments_position[x-1])
 
     return (
         students_years,
@@ -46,13 +58,3 @@ def generate_data(N: int, num_dormitories: int, num_departments: int) -> Tuple[
         departments_position
     )
 
-# Przykład użycia
-N = 50  # Liczba studentów
-num_dormitories = 10  # Liczba akademików
-num_departments = 4  # Liczba wydziałów
-data = generate_data(N, num_dormitories, num_departments)
-
-# dane
-(students_years, students_disability, students_priority_lists,
- students_departments, dormitorys_capacity, dormitory_position,
- departments_position) = data
